cb13316a49c920b23d91e4ce24beda2b4c000f37
Lautaro <lautarollanosnahuel@gmail.com>
2022-08-31 18:51:14 -0300

    db

--- itbank/ClienteSaldo/serializers.py
+++ itbank/ClienteSaldo/serializers.py
@@ -4,7 +4,6 @@ from .models import Cliente
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        #indicamos que use todos los campos
         fields = "__all__"
 #les decimos cuales son los de solo lectura
 #read_only_fields = ("id","created_at","updated_at",)
